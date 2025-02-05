3fe54181e61c866e68f44fafc785b96d58034182
Sitao Zheng <zhengst1991@gmail.com>
2025-02-05 01:18:46 -0800

    Initial commit

---
+++ src/main/java/com/example/skillpath_pro/SkillpathProApplication.java
@@ -0,0 +1,13 @@
+package com.example.skillpath_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillpathProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillpathProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skillpath_pro/SkillpathProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillpath_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillpathProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
